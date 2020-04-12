331ff67a81915898e66a40e7598b60c3c98c3390
pravanop <pravanomprakash.181mt035@nit.edu.in>
2020-04-12 15:58:04 +0530

    stocks.py

---
+++ Child_classes.py
@@ -0,0 +1,6 @@
+from Intent import *
+from Stocks import *
+class Stocks(Intent):
+	function=function
+        
+	
---
+++ Stocks.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import os
+from Intent import *
+# In[1]:
+chrome_options= Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_driver = os.getcwd() +"\\chromedriver.exe"
+
+#company=input("Give the company name:")
+
+
+# In[2]:
+
+class Stocks_func(Intent):
+    
+    def stock_price(self,query):
+        driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
+        driver.get("https://www.google.com") 
+        import time 
+        time.sleep(4)
+        search_bar = driver.find_element_by_name("q")
+        search_bar.clear()
+        search_bar.send_keys(query)
+        search_bar.submit()
+        time.sleep(3)
+        price= driver.find_element_by_css_selector("span[jsname='vWLAgc']")
+        percent=driver.find_element_by_css_selector("span[jsname='rfaVEf']")
+        return 'It is at '+ price.text + 'and ' + percent.text 
+
+# In[ ]:
+
+
+#print(stock_price(company))
+
